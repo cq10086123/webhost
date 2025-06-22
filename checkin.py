49dbfd6205e5c4c809932dca9971da1626bd93e5
cq10086123 <105488660+cq10086123@users.noreply.github.com>
2025-06-22 09:20:48 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -1,6 +1,17 @@
 import os
 import requests
-
+def send_telegram_message(message):
+    bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
+    print(bot_token)
+    chat_id = os.environ.get('TELEGRAM_CHAT_ID')
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": message,
+        "parse_mode": "Markdown"
+    }
+    response = requests.post(url, json=payload)
+    return response.json()
 def denglu():
     raw_accounts = os.environ.get("ACCOUNTS", "")
     account_entries = [entry.strip() for entry in raw_accounts.split(";") if entry.strip()]
